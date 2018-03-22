3e7a811d4c47db583ea1cc11ca5c5e680da50beb
mountwest <32386492+mountwest@users.noreply.github.com>
2018-03-22 15:22:30 +0100

    Listview and side menu working

--- www/js/app.js
+++ www/js/app.js
@@ -5,20 +5,45 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    })
+
+    function ContentController($scope, $ionicSideMenuDelegate) {
+    $scope.toggleRight = function () {
+      $ionicSideMenuDelegate.toggleRight();
+    };
     }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
-})
+
+  })
+
+  .controller("ListController", function($scope){
+    $scope.rooms = [
+      {id: 001, size: 1, type: "Small", price: 299},
+      {id: 022, size: 1, type: "Small", price: 299},
+      {id: 103, size: 1, type: "Big", price: 799},
+      {id: 404, size: 2, type: "Small", price: 699},
+      {id: 305, size: 2, type: "Big", price: 899},
+      {id: 306, size: 4, type: "Big", price: 1199},
+      {id: 207, size: 4, type: "Big", price: 1299},
+      {id: 110, size: 8, type: "Big", price: 299},
+    ];
+
+    $scope.exchangeRate = [
+      {eur: 9.2},
+      {usd: 7.2},
+    ]
+  })
