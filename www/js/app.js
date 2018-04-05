90b2ee6da5eaec22d3e54e1709d17d74de6fe42a
mountwest <32386492+mountwest@users.noreply.github.com>
2018-04-05 12:47:28 +0200

    mt.west@protonmail.com

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,41 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+  .config(function ($stateProvider, $urlRouterProvider) {
+
+    $stateProvider
+      .state("root", {
+        url: "/root",
+        templateUrl: "/templates/root.html",
+        abstract: true
+      }) // root end
+
+      // ROOMS // 
+      .state("root.rooms", {
+        url: "/rooms",
+        views: {
+          "rooms-view": {
+            templateUrl: "/templates/rooms.html",
+            controller: "ListController"
+          }
+        }
+      })
+
+      // ROOM //
+      .state("root.room", {
+        url: "/rooms/:ID",
+        views: {
+          "rooms-view": {
+            templateUrl: "/templates/room.html",
+            controller: "ListController"
+          }
+        }
+      })
+
+    $urlRouterProvider.otherwise("/root/rooms");
+
+  })
+
   .run(function ($ionicPlatform) {
     $ionicPlatform.ready(function () {
       if (window.cordova && window.cordova.plugins.Keyboard) {
@@ -23,32 +58,81 @@ angular.module('starter', ['ionic'])
     })
 
     function ContentController($scope, $ionicSideMenuDelegate) {
-    $scope.toggleRight = function () {
-      $ionicSideMenuDelegate.toggleRight();
-    };
+      $scope.toggleRight = function () {
+        $ionicSideMenuDelegate.toggleRight();
+      };
     }
 
   })
 
-  .controller("ListController", function($scope){
-    $scope.rooms = [
-      {id: 001, size: 1, type: "Small", price: 299},
-      {id: 022, size: 1, type: "Small", price: 299},
-      {id: 103, size: 1, type: "Big", price: 799},
-      {id: 404, size: 2, type: "Small", price: 699},
-      {id: 305, size: 2, type: "Big", price: 899},
-      {id: 306, size: 4, type: "Big", price: 1199},
-      {id: 207, size: 4, type: "Big", price: 1299},
-      {id: 110, size: 8, type: "Big", price: 299},
-    ];
+  .controller("ListController", function ($scope, $http, $stateParams) {
 
-    // API's for exchange 
-    //
-    // http://fixer.io/
-    // http://rest/.com/currency_convert
+    $http.get('http://localhost/crossplatform/get.php')
+      .success(function (data) {
+        $scope.rooms = data;
+        console.log(data);
+      })
 
-    $scope.exchangeRate = [
-      {eur: 9.2},
-      {usd: 7.2},
+    $scope.ID = $stateParams.ID;
+
+    $http.get('http://data.fixer.io/api/latest?access_key=70465cfb10a0683063762ffcd4dcbb40')
+      .success(function (data) {
+        $scope.exchange = data;
+        console.log(data);
+      })
+
+    $scope.symbols = [
+      {eur: "€"},
+      {usd: "$"},
     ]
+
+    console.log($scope.symbols[0]);
+
+    $scope.currencySelected = function (selected){
+      $scope.currency = $scope.symbols[selected];
+    }
+
+  })
+
+  .controller("FormController", function ($scope, $http, $filter) {
+
+    $scope.data = {};
+
+    $scope.today = $filter("date")(Date.now(),'yyyy-MM-dd');
+    
+    $scope.startdateFormat = function (startdate) {
+      $scope.data.startdate = startdate.toLocaleDateString();
+    }
+
+    $scope.enddateFormat = function (enddate) {
+      $scope.data.enddate = enddate.toLocaleDateString();
+    }
+
+    $scope.diffDate = function (data) {
+
+      var checkin = new Date(data.startdate);
+      var checkout = new Date(data.enddate);
+      var timeDiff = Math.abs(checkout.getTime() - checkin.getTime());
+      var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+      var diffD = parseInt(diffDays);
+      if (!diffD || diffD == 0) { 
+          diffD = 1;
+        };
+      $scope.diffD = diffD;
+      return diffD;
+    };
+
+    $scope.submit = function (data) {
+
+      console.log(data.enddate - data.startdate);
+      console.log(data);
+      data.id = $scope.ID;
+
+      var url = "http://localhost/crossplatform/submit.php"
+      $http.post(url, data)
+        .then(function (response) {
+          $scope.response = response;
+          console.log($scope.response);
+        })
+    }
   })
